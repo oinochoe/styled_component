26d8e3b2a6fc9494502b89567ecba790049dcadd
oinochoe <oinochoe@naver.com>
2020-04-16 09:27:59 +0900

    과거의 방식

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, { Component, Fragment } from 'react';
+import './App.css';
 
-function App() {
-    return <></>;
+class App extends Component {
+    render() {
+        return (
+            <Fragment>
+                <button className='button button--success'>Hello</button>
+                <button className='button button--danger'>Hello</button>
+            </Fragment>
+        );
+    }
 }
 
 export default App;

e4bdd1f693842de29bc8b1dd46467edc1b991c05
oinochoe <oinochoe@naver.com>
2020-04-17 09:35:55 +0900

    css mixin

--- src/App.js
+++ src/App.js
@@ -11,6 +11,14 @@ const GlobalStyle = createGlobalStyle`
     }
 `;
 
+// mixin
+const awesomeCard = css`
+    box-shadow: 0 4px 6px rgba(50, 50, 93, 0.11), 0 1px 3px rgba(0, 0, 0, 0.08);
+    background-color: white;
+    border-radius: 10px;
+    padding: 20px;
+`;
+
 const Button = styled.button`
     min-width: 120px;
     padding: 5px;
@@ -38,7 +46,7 @@ const Button = styled.button`
 const Container = styled.div`
     height: 100vh;
     width: 100%;
-    background-color: skyblue;
+    ${awesomeCard}
 `;
 
 const Anchor = styled(Button.withComponent('a'))`
@@ -57,7 +65,8 @@ const rotation = keyframes`
 const Input = styled.input.attrs({
     required: true,
 })`
-    border-radius: 5px;
+    border: none;
+    ${awesomeCard}
 `;
 
 class App extends Component {
@@ -65,7 +74,9 @@ class App extends Component {
         return (
             <React.Fragment>
                 <GlobalStyle />
-                <Input placeholder='hello' />
+                <Container>
+                    <Input placeholder='hello' />
+                </Container>
                 {/* <Container>
                     <Button>Hello</Button>
                     <Button danger duration={1} delay={2}>
